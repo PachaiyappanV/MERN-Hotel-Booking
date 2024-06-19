312fa7a070cca7b6bc46a3bb4e454bbf1fdf00f2
PachaiyappanV <pachaiyappan394@gmail.com>
2024-06-19 21:39:02 +0530

    feat: add routes using react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,13 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/Layout";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout>content</Layout>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
