b7aea6e064eb26235f0ac16e04ff339c15143a4f
PachaiyappanV <pachaiyappan394@gmail.com>
2024-06-19 22:41:47 +0530

    feat: create and add hero component

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+  return (
+    <div className="bg-blue-800 pb-16">
+      <div className="container mx-auto flex flex-col gap-2">
+        <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold text-white">
+          Find Your next stay
+        </h1>
+        <p className="text-white md:text-xl lg:text-2xl">
+          Searh low prices on hotels for your dream vacation...
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Hero;
