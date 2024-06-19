af9314d208826a723bbaf4f95e7f8bf2bacfd818
PachaiyappanV <pachaiyappan394@gmail.com>
2024-06-19 22:10:09 +0530

    feat:create and add header componet

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="bg-blue-800 py-6">
+      <div className="container mx-auto flex justify-between items-center">
+        <span className="text-3xl text-white font-bold tracking-tight">
+          <Link to="/">GreenHolidays.com</Link>
+        </span>
+        <span className="flex space-x-2">
+          <Link
+            to="/sign-in"
+            className="text-blue-600 font-bold px-2 py-1 rounded-sm hover:bg-gray-100 "
+          >
+            Sign In
+          </Link>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import Header from "../components/Header";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Layout = ({ children }: Props) => {
-  return <div>{children}</div>;
+  return (
+    <div className=" flex flex-col min-h-screen">
+      <Header />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
