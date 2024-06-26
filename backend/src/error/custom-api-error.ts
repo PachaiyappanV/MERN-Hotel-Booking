15fa8533200fd0df0957f357d3e41b17513fb579
PachaiyappanV <pachaiyappan394@gmail.com>
2024-06-26 22:06:24 +0530

    feat: create BadRequestError class

--- backend/src/error/custom-api-error.ts
+++ backend/src/error/custom-api-error.ts
@@ -1,6 +1,11 @@
 class CustomAPIError extends Error {
-  constructor(message: string) {
+  statusCode: number;
+  constructor(message: string, statusCode: number) {
     super(message);
+    this.statusCode = statusCode;
+
+    // Set the prototype explicitly for extending Error
+    Object.setPrototypeOf(this, CustomAPIError.prototype);
   }
 }
 
