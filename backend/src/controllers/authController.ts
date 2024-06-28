7a52b42f22bac3dbf119900b9bf9c8ddae5b7c6c
PachaiyappanV <pachaiyappan394@gmail.com>
2024-06-29 00:05:15 +0530

    feat(auth): implement user register functionality with JWT and cookies generation

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,10 +1,24 @@
 import { Request, Response } from "express";
 import User from "../models/User";
+import { attachCookiesToResponse, createTokenUser } from "../utils";
+import { StatusCodes } from "http-status-codes";
 
 const register = async (req: Request, res: Response) => {
   const user = await User.create(req.body);
+  const tokenUser = createTokenUser(user);
 
-  res.json({ user });
+  attachCookiesToResponse(res, tokenUser);
+  res.status(StatusCodes.CREATED).json({
+    status: "success",
+    data: {
+      message: "user registered successfully",
+      user: {
+        name: user.firstName + " " + user.lastName,
+        email: user.email,
+        id: user._id,
+      },
+    },
+  });
 };
 
 const login = async (req: Request, res: Response) => {
